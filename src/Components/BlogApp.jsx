ba2425bbf2d792439941428f994de96dad1ab078
Ravi Shukla <rshuklabbb@gmail.com>
2022-01-07 16:29:00 +0530

    Added loading indicator

--- src/Components/BlogApp.jsx
+++ src/Components/BlogApp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import BlogCard from "./BlogCard/BlogCard";
 import Pagination from "./Pagination/Pagination";
 import SearchBar from "./SearchBar/SearchBar";
+import styles from "./BlogApp.module.css";
+import LoadingIndicator from "./LoadingIndicator/LoadingIndicator";
 
 const BlogApp = () => {
     const [isLoading, setIsLoading] = React.useState(true);
@@ -9,6 +11,7 @@ const BlogApp = () => {
     const [blogs, setBlogs] = React.useState(null);
     const [query, setQuery] = React.useState("");
     const [page, setPage] = React.useState(1);
+    const [totalPage, setTotalPage] = React.useState(1);
 
     const fetchBlogs = async (query = null) => {
         try {
@@ -17,6 +20,8 @@ const BlogApp = () => {
             if (query) url += `?q=${query}`
             let data = await fetch(url);
             data = await data.json();
+            setTotalPage(Math.floor(data.length/10));
+            setPage(1);
             setBlogs(data);
         } catch (error) {
             console.log(error);
@@ -31,17 +36,17 @@ const BlogApp = () => {
     }, [query]);
 
     return (
-        <div className="App">
+        <div className={styles.root}>
             <SearchBar setQuery={setQuery} />
             {
                 isLoading ? (
-                    <div>...loading</div>
+                    <LoadingIndicator />
                 ) : isError ? (
-                    <div>Something went wrong! check console for more info!</div>
+                    <div className={styles.error} >Something went wrong! check console for more info!</div>
                 ) : (
                     // handle pagination using filter
-                    <div>
-                        <div>
+                    <div className={styles.body} >
+                        <div className={styles.blog} >
                             {
                                 blogs
                                     .filter((_, index) => index >= ( page - 1 ) * 10 && index < page * 10)
@@ -56,7 +61,7 @@ const BlogApp = () => {
                             }
                         </div>
                         <Pagination
-                            totalPage={10}
+                            totalPage={totalPage}
                             currPage={page}
                             onPageChange={(page)=>setPage(page)}
                         />
