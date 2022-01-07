1bac2ffc8a40ee8c08b726b4151427964203f8d5
Ravi Shukla <rshuklabbb@gmail.com>
2022-01-07 15:44:14 +0530

    Added styles to the blog card

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,11 +1,13 @@
+import styles from "./BlogCard.module.css";
+
 const BlogCard = ({
     title,
     body,
     userId
 }) => {
     return (
-        <div>
-            <div>
+        <div className={styles.root}>
+            <div className={styles.heading}>
                 <h3>{title}</h3>
                 <h5>By - {userId}</h5>
             </div>
